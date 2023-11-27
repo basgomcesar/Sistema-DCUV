bc08996d19467874e6bf627a1581511a447a8857
CarlosRodriguezJr <carlos-rodriguezjr@hotmail.com>
2023-11-27 08:06:43 -0600

    INICIO DE SESION DAO (INCOMPLETO)

--- SistemaDCUV/src/sistemadcuv/controladores/FXMLInicioSesionController.java
+++ SistemaDCUV/src/sistemadcuv/controladores/FXMLInicioSesionController.java
@@ -1,6 +1,5 @@
 package sistemadcuv.controladores;
 
-import Utils.Utilidades;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -9,6 +8,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import sistemadcuv.modelo.pojo.RespuestaLogin;
+import sistemadcuv.utils.Utilidades;
 
 
 public class FXMLInicioSesionController implements Initializable {
@@ -23,7 +24,13 @@ public class FXMLInicioSesionController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-    }    
+    }
+    
+    private RespuestaLogin verificarUsuario(){
+        RespuestaLogin usuarioAutentificado = null;
+                //TODO
+        return usuarioAutentificado;
+    }
 
     @FXML
     private void iniciarSesion(ActionEvent event) {
@@ -33,13 +40,13 @@ public class FXMLInicioSesionController implements Initializable {
                    
                 }
             }else{
-                Utilidades.mostrarDialogo("Formato invalido",
+                Utilidades.mostrarAletarSimple("Formato invalido",
                         "El usuario no coincide con el formato de una matricula o numero de personal\n"
                                 + "Ej.matricula valida: 'zsXXXXXXX", 
                         Alert.AlertType.INFORMATION);
             }
         }else{
-            Utilidades.mostrarDialogo("Campos vacios", 
+            Utilidades.mostrarAletarSimple("Campos vacios", 
                     "Por favor llene los campos faltantes", 
                     Alert.AlertType.INFORMATION);
         }
