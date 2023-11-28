19dc0aade721095fef6f8870d72637986579ae92
CarlosRodriguezJr <carlos-rodriguezjr@hotmail.com>
2023-11-28 10:49:52 -0600

    PRIMEROS CASOS DE USO

--- SistemaDCUV/src/sistemadcuv/controladores/FXMLListadoDeActividadesController.java
+++ SistemaDCUV/src/sistemadcuv/controladores/FXMLListadoDeActividadesController.java
@@ -1,16 +1,9 @@
 package sistemadcuv.controladores;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
