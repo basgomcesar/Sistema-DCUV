2b5d108069c76604c6b20417e32c5aaa4a44f70a
Mauhds <149717254+Mauhds@users.noreply.github.com>
2023-11-27 18:16:32 -0600

    Cambio FXMLParticipantesProyecto

--- SistemaDCUV/src/sistemadcuv/controladores/FXMLInicioSesionController.java
+++ SistemaDCUV/src/sistemadcuv/controladores/FXMLInicioSesionController.java
@@ -1,18 +1,11 @@
 package sistemadcuv.controladores;
 
-import java.io.IOException;
-import sistemadcuv.utils.Utilidades;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
